a2aa8bc6fe259f80ec5477f23ab05b6f64494d7b
aravind23457 <yvaravind01@gmail.com>
2021-05-29 20:03:50 +0530

    else if days program

---
+++ Elseif1.java
@@ -0,0 +1,47 @@
+class ElseIf1
+{
+public static void main(String[] args)
+{
+String name="mon";
+if(name.equals("mon")){
+
+System.out.println("mon");
+ 
+}else if(name.equals("tue"))
+
+{
+
+System.out.println("tue");
+
+}else if(name.equals("wed"))
+{
+
+System.out.println("wed");
+
+}else if(name.equals("thurs")) 
+{
+
+System.out.println("thurs");
+
+}else if(name.equals("fri"))
+{
+
+System.out.println("fri");
+
+}else if(name.equals("sat"))
+{
+
+System.out.println("sat");
+
+}else{
+System.out.println("sun");}
+}
+}
+
+
+
+
+
+
+
+
