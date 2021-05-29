f79fd239c48be695f7f8cd987b51bc4e938cbffe
aravind23457 <yvaravind01@gmail.com>
2021-05-29 20:03:08 +0530

    else if month program

---
+++ ElseIf.java
@@ -0,0 +1,47 @@
+class ElseIf
+{
+public static void main(String[] args)
+{
+String name="jan";
+if(name.equals("jan")){
+
+System.out.println("jan");
+ 
+}else if(name.equals("feb"))
+
+{
+
+System.out.println("feb");
+
+}else if(name.equals("mar"))
+{
+
+System.out.println("mar");
+
+}else if(name.equals("april")) 
+{
+
+System.out.println("april");
+
+}else if(name.equals("may"))
+{
+
+System.out.println("may");
+
+}else if(name.equals("june"))
+{
+
+System.out.println("june");
+
+}else{
+System.out.println("not found");}
+}
+}
+
+
+
+
+
+
+
+
